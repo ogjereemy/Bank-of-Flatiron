3372074455bcb353105a91d9a2b58392366089b2
ogola <ogolajeremy601@gmail.com>
2024-04-27 13:13:40 +0300

    adds table rows and data

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,28 +1,28 @@
-// import React from 'react';
+import React from 'react';
 
-// function TransactionTable({ transactions }){
-//   return (
-//     <table>
-//       <thead>
-//         <tr>
-//           <th>Date</th>
-//           <th>Description</th>
-//           <th>Category</th>
-//           <th>Amount (Ksh)</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {transactions.map((transaction, index) => (
-//           <tr key={index}>
-//             <td>{transaction.date}</td>
-//             <td>{transaction.description}</td>
-//             <td>{transaction.category}</td>
-//             <td>{transaction.amount}</td>
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   );
-// }
+function TransactionTable({ transactions }){
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount (Ksh)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction, index) => (
+          <tr key={index}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
-// export default TransactionTable;
+export default TransactionTable;
