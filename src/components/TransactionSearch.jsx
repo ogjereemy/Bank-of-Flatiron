49735e8fc6d2ecd8ccb7d6838154c8cf3cf67ed2
ogola <ogolajeremy601@gmail.com>
2024-04-27 13:11:31 +0300

    adds transaction search input

--- src/components/TransactionSearch.jsx
+++ src/components/TransactionSearch.jsx
@@ -1,16 +1,16 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// function TransactionSearch({ onSearch }){
-//   const [searchTerm, setSearchTerm] = useState('');
+function TransactionSearch({ onSearch }){
+  const [searchTerm, setSearchTerm] = useState('');
 
-//   function handleChange(e){
-//     setSearchTerm(e.target.value);
-//     onSearch(e.target.value);
-//   };
+  function handleChange(e){
+    setSearchTerm(e.target.value);
+    onSearch(e.target.value);
+  };
 
-//   return (
-//     <input id='search' type="text" value={searchTerm} onChange={handleChange} placeholder="Search transactions..." />
-//   );
-// }
+  return (
+    <input id='search' type="text" value={searchTerm} onChange={handleChange} placeholder="Search transactions..." />
+  );
+}
 
-// export default TransactionSearch;
+export default TransactionSearch;
